395e17698ce580b81a0aeb9e0d1bd04862b3beda
LadyK <static.katherine@gmail.com>
2018-04-03 15:56:17 -0400

    Amended code for Shift Register
    Use the Liquid Crystal library that is here on github, to replace the Liquid Crystal library that ships with the Arduino. The version here has an SPI capabilities, which this code works with. Note the wiring.

--- shiftRegister/HelloWorld_SPI_LCD/HelloWorld_SPI/HelloWorld_SPI.ino/HelloWorld_SPI.ino.ino
+++ shiftRegister/HelloWorld_SPI_LCD/HelloWorld_SPI/HelloWorld_SPI.ino/HelloWorld_SPI.ino.ino
@@ -1,4 +1,6 @@
 #include <SPI.h>
+// include the library code:
+#include <LiquidCrystal.h>
 
 /*
   LiquidCrystal Library - Hello World
@@ -11,18 +13,32 @@
   This sketch prints "Hello World!" to the LCD
   and shows the time.
 
-  The circuit:
-   LCD RS pin to digital pin 12
-   LCD Enable pin to digital pin 11
-   LCD D4 pin to digital pin 5
-   LCD D5 pin to digital pin 4
-   LCD D6 pin to digital pin 3
-   LCD D7 pin to digital pin 2
-   LCD R/W pin to ground
-   10K resistor:
-   ends to +5V and ground
-   wiper to LCD VO pin (pin 3)
+  The circuit wiring:
 
+  LCD pin 1 : ground
+  LCD pin 2 : positive voltage
+  LCD pin 3 : middle pin of 10K potentiometer
+  LCD pin 4 : Shift Register pin 1
+  LCD pin 5 : Ground
+  LCD pin 6 : Shift Register pin 3
+  LCD pin 7 - 10 : no connection
+  LCD pin 11 : Shift Register pin 4
+  LCD pin 12 : Shift Register pin 5
+  LCD pin 13 : Shift Register pin 6
+  LCD pin 14 : Shift Register pin 7
+  LCD pin 15 : 330 ohm pull up resistor
+  LCD pin 16 : ground
+
+  Shift Register pin 2 : No connection
+  Shift Register pin 9 : No connection
+  Shift Register pin 10 : positive voltage
+  Shift Register pin 11 : Arduino pin 13
+  Shift Register pin 12 : Arduino pin 9
+  Shift Register pin 13 : ground
+  Shift Register pin 14 : Arduino pin 11
+  Shift Register pin 15 : no connection
+  Shift Register pin 16 : positive voltage
+  
   Library originally added 18 Apr 2008
   by David A. Mellis
   library modified 5 Jul 2009
@@ -34,13 +50,17 @@
   modified 12 Feb 2012
   by Juan Hernandez
 
+  modified 3 April 2018
+  by Katherine Bennett
+
   This example code is in the public domain.
 
   http://www.arduino.cc/en/Tutorial/LiquidCrystal
 */
 
-// include the library code:
-#include <LiquidCrystal.h>
+
+
+
 
 // initialize the library with the number of the sspin
 //(or the latch pin of the 74HC595)
@@ -51,14 +71,16 @@ void setup() {
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.print("hello, world!");
-  delay(2000);
-  lcd.clear();
 }
 
 void loop() {
+  lcd.setCursor(0, 0);
+  lcd.print("hello, world!");
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
   lcd.print(millis() / 1000);
+
+
 }
